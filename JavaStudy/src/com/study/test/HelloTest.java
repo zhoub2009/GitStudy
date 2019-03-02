ede850d63de3876ae5a50e32fb9f5d96ee57244c
zhoub2009 <zhoub2009@sina.cn>
2019-03-02 21:49:42 +0800

    分支dev测试2

--- JavaStudy/src/com/study/test/HelloTest.java
+++ JavaStudy/src/com/study/test/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("This is my first Git Program!! ");
 		//this is the dev branch
+		//this is the dev branch test1 
 	}
 }
