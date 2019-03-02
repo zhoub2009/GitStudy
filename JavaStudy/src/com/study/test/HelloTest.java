da4893ea567c597d8ece71c2825b3529b8fb6d24
zhoub2009 <zhoub2009@sina.cn>
2019-03-02 21:44:35 +0800

    分支修改

--- JavaStudy/src/com/study/test/HelloTest.java
+++ JavaStudy/src/com/study/test/HelloTest.java
@@ -3,5 +3,6 @@ package com.study.test;
 public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("This is my first Git Program!! ");
+		//this is the dev branch
 	}
 }
