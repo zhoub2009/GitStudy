b732562f3fad9fdbafc4fca4c9ae8351e1f6d189
zhoub2009 <zhoub2009@sina.cn>
2019-03-02 21:41:51 +0800

    修改
    Change-Id: Ic47888c5e5dbff107b0809a7364d3052880c37f3
    Signed-off-by: zhoub2009 <zhoub2009@sina.cn>

---
+++ JavaStudy/src/com/study/test/HelloTest.java
@@ -0,0 +1,8 @@
+package com.study.test;
+
+public class HelloTest {
+	public static void main(String[] args) {
+		System.out.println("This is my first Git Program!! ");
+		System.err.println("this is dev test 3333");
+	}
+}
